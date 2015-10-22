a2cbba1bfd34c6734674b2dba5769cd73aacb6bc
WEI WU <wei@teambanjo.com>
2015-10-22 10:57:58 -0700

    update deps

--- metadata.rb
+++ metadata.rb
@@ -6,8 +6,6 @@ description      'Installs graphite-api'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.2'
 
-recipe 'graphite-api', 'Installs graphite-api.'
-
 supports 'debian'
 supports 'ubuntu'
 
